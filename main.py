1f11faa4a491dc93a682c06c3e4e28ffbba706b9
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2024-10-18 11:09:40 -0300

    Refatoração

--- main.py
+++ main.py
@@ -1,117 +1,105 @@
 import hashlib
-import os
 import shutil
 import sys
 from datetime import datetime, timedelta
+from pathlib import Path
+
 
 # Console colors
-W = '\033[0m'  # white (normal)
-R = '\033[31m'  # red
-G = '\033[32m'  # green
-O = '\033[33m'  # orange
-B = '\033[34m'  # blue
-P = '\033[35m'  # purple
-C = '\033[36m'  # cyan
-GR = '\033[37m'  # gray
+class Colors:
+    W = '\033[0m'  # white (normal)
+    R = '\033[31m'  # red
+    G = '\033[32m'  # green
+    O = '\033[33m'  # orange
+    B = '\033[34m'  # blue
+    P = '\033[35m'  # purple
+    C = '\033[36m'  # cyan
+    GR = '\033[37m'  # gray
 
 
-def calcular_hash(arquivo, algoritmo="MD5"):
+def calcular_hash(arquivo: Path, algoritmo="MD5") -> str:
+    """Calcula o hash do arquivo dado utilizando o algoritmo especificado."""
     hash_alg = hashlib.new(algoritmo)
 
-    with open(arquivo, "rb") as f:
-        while True:
-            bloco = f.read(4096)
-            if not bloco:
-                break
+    with arquivo.open("rb") as f:
+        for bloco in iter(lambda: f.read(4096), b""):
             hash_alg.update(bloco)
 
     return hash_alg.hexdigest()
 
 
-def contar_arquivos(diretorio):
+def contar_arquivos(diretorio: Path) -> int:
+    """Conta quantos arquivos existem dentro de um diretório, incluindo subdiretórios."""
     total_arquivos = 0
-
     print('Verificando quantidade de arquivos ...')
-    # Percorre o diretório e seus subdiretórios
-    for _, _, arquivos in os.walk(diretorio):
-        total_arquivos += len(arquivos)  # Conta a quantidade de arquivos
-        print(f'\r{total_arquivos} arquivos encontrados ...', end='')
-    print(f'\r{total_arquivos} arquivos encontrados ... [ {G}OK{W} ]')
 
+    for arquivos in diretorio.rglob('*'):
+        if arquivos.is_file():
+            total_arquivos += 1
+            print(f'\r{total_arquivos} arquivos encontrados ...', end='')
+
+    print(f'\r{total_arquivos} arquivos encontrados ... [ {Colors.G}OK{Colors.W} ]')
     return total_arquivos
 
 
-def replica_diretorios(origem, destino):
-    # Verifica se o diretório existe
-    if not os.path.isdir(origem):
-        print(f"{R}Erro{W}: O diretório '{origem}' não existe.")
+def replica_diretorios(origem: Path, destino: Path):
+    """Replica arquivos de um diretório origem para o diretório destino, mantendo a estrutura de diretórios."""
+    if not origem.is_dir():
+        print(f"{Colors.R}Erro{Colors.W}: O diretório '{origem}' não existe.")
         return
 
     total_arquivos = contar_arquivos(origem)
     arquivo = 0
 
-    for raiz, diretorios, arquivos in os.walk(origem):
-        for nome_arquivo in arquivos:
+    for arquivo_origem in origem.rglob('*'):
+        if arquivo_origem.is_file():
             arquivo += 1
             print(f'\rCopiando arquivo {arquivo} de {total_arquivos} ...', end='')
-            sulfixo = raiz.split(origem)[1]
-            caminho_final = f'{destino}{sulfixo}'
-            os.makedirs(caminho_final, exist_ok=True)
-            origem_arquivo = os.path.join(raiz, nome_arquivo)
-            destino_arquivo = os.path.join(caminho_final, nome_arquivo)
-            shutil.copy(origem_arquivo, destino_arquivo)
+            destino_final = destino / arquivo_origem.relative_to(origem)
+            destino_final.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy(arquivo_origem, destino_final)
 
-    print(f'\rCopiados {arquivo} de {total_arquivos} arquivos ... [ {G}OK{W} ]')
+    print(f'\rCopiados {arquivo} de {total_arquivos} arquivos ... [ {Colors.G}OK{Colors.W} ]')
 
 
-def tempo_do_arquivo_maior_que(caminho_arquivo, dias=2):
-    # Obtém o tempo de modificação do arquivo
-    tempo_modificacao = os.path.getmtime(caminho_arquivo)
-    # Converte o tempo de modificação
-    data_modificacao = datetime.fromtimestamp(tempo_modificacao)
-    # Pega a data de X dias atrás
-    dois_dias_atras = datetime.now() - timedelta(days=dias)
-
-    # Verifica se o arquivo foi modificado há mais de X dias
-    return data_modificacao < dois_dias_atras
+def tempo_do_arquivo_maior_que(arquivo: Path, dias: int = 2) -> bool:
+    """Verifica se o tempo de modificação de um arquivo é maior que X dias."""
+    data_modificacao = datetime.fromtimestamp(arquivo.stat().st_mtime)
+    return data_modificacao < (datetime.now() - timedelta(days=dias))
 
 
-def limpa_arquivos_copiados(origem, destino):
-    for raiz, diretorios, arquivos in os.walk(origem):
-        for nome_arquivo in arquivos:
-            sulfixo = raiz.split(origem)[1]
-            caminho_final = f'{destino}{sulfixo}'
+def limpa_arquivos_copiados(origem: Path, destino: Path):
+    """Verifica a integridade dos arquivos copiados e remove os arquivos antigos do diretório de origem."""
+    for arquivo_origem in origem.rglob('*'):
+        if arquivo_origem.is_file():
+            destino_arquivo = destino / arquivo_origem.relative_to(origem)
 
-            origem_arquivo = os.path.join(raiz, nome_arquivo)
-            destino_arquivo = os.path.join(caminho_final, nome_arquivo)
+            print(f'\rVerificando integridade {arquivo_origem.name} ...', end='')
 
-            print(f'\rVerificando integridade {nome_arquivo} ...', end='')
+            if destino_arquivo.exists():
+                hash_o = calcular_hash(arquivo_origem)
+                hash_d = calcular_hash(destino_arquivo)
+                integridade = hash_o == hash_d
 
-            hash_o = calcular_hash(origem_arquivo)
-            hash_d = calcular_hash(destino_arquivo)
-
-            integridade = hash_o == hash_d
-
-            if integridade:
-                print(f'\rVerificando integridade {nome_arquivo} ... [ {G}OK{W} ]')
-            else:
-                print(f'\rVerificando integridade {nome_arquivo} ... [ {R}ERROR{W} ]')
-
-            if integridade and tempo_do_arquivo_maior_que(origem_arquivo):
-                print(f'Removendo arquivo antigo: {R}{nome_arquivo}{W}')
-                os.remove(origem_arquivo)
+                if integridade:
+                    print(f'\rVerificando integridade {arquivo_origem.name} ... [ {Colors.G}OK{Colors.W} ]')
+                    if tempo_do_arquivo_maior_que(arquivo_origem):
+                        print(f'Removendo arquivo antigo: {Colors.R}{arquivo_origem.name}{Colors.W}')
+                        arquivo_origem.unlink()
+                else:
+                    print(f'\rVerificando integridade {arquivo_origem.name} ... [ {Colors.R}ERROR{Colors.W} ]')
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print('Faltam parâmetros! Certifique-se de passar os diretórios de origem e destino!')
-        print(f'{R}ABORTANDO ...{W}')
-        exit(-1)
+        print(f'Faltam parâmetros! Certifique-se de passar os diretórios de origem e destino!')
+        print(f'{Colors.R}ABORTANDO ...{Colors.W}')
+        sys.exit(-1)
 
-    origem = sys.argv[1]
-    destino = sys.argv[2]
+    origem = Path(sys.argv[1])
+    destino = Path(sys.argv[2])
 
     replica_diretorios(origem, destino)
     limpa_arquivos_copiados(origem, destino)
 
-    print(f'{G}============ Programa finalizado ============{W}')
+    print(f'{Colors.G}============ Programa finalizado ============{Colors.W}')
